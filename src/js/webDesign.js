96e8eb3f52bc7c3902a25bfbe5bd549e7a761090
Maximilian Coghlan <max.coghlan@icloud.com>
2020-11-03 11:32:53 +0000

    portfolio section

--- src/js/webDesign.js
+++ src/js/webDesign.js
@@ -1,3 +1,4 @@
+// TOP BANNER ICON ROLLOVERS ON CLICK FOR TOUCH SCREENS
 const icons = document.querySelectorAll(".icon");
 
 icons.forEach((icon) => {
@@ -8,3 +9,55 @@ icons.forEach((icon) => {
     icon.classList.toggle("active");
   });
 });
+
+// PORTFOLIO GSAP
+let tl = gsap.timeline({
+  scrollTrigger: {
+    trigger: "#portfolio-container",
+    start: "center center",
+    end: "+=3000px",
+    scrub: true,
+    pin: true,
+  },
+});
+
+tl.to("#computer", {
+  width: 700,
+  height: "100%",
+})
+
+  .to("#turn-on", {
+    opacity: 1,
+  })
+
+  .to("#laborjee", {
+    opacity: 1,
+  })
+
+  .to("#laborjee", {
+    y: "-461px",
+  })
+
+  .to("#olive", {
+    x: "-359px",
+  })
+
+  .to("#azadi", {
+    x: 0,
+  })
+
+  .to("#azadi", {
+    y: "-233px",
+  })
+
+  .to("#creperie", {
+    x: 0,
+  })
+
+  .to("#creperie", {
+    y: "-344px",
+  })
+
+  .to("#turn-off", {
+    opacity: 1,
+  });
