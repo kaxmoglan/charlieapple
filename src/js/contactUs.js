6d2d91729e40c3b5ba45c156b8ae3b37aa93462c
Maximilian Coghlan <max.coghlan@icloud.com>
2020-11-04 23:36:53 +0000

    contact

---
+++ src/js/contactUs.js
@@ -0,0 +1,19 @@
+const textInputs = document.querySelectorAll(".text-input");
+
+textInputs.forEach((textInput) => {
+  const label = textInput.getElementsByTagName("label");
+  const input = textInput.getElementsByTagName("input");
+  const activeline = textInput.getElementsByClassName("activeline");
+
+  input[0].addEventListener("focusin", () => {
+    activeline[0].classList.add("active");
+    label[0].classList.add("active");
+  });
+
+  input[0].addEventListener("focusout", () => {
+    activeline[0].classList.remove("active");
+    if (!input[0].value) {
+      label[0].classList.remove("active");
+    }
+  });
+});
