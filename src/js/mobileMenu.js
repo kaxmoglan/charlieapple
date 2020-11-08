ebaef61e7cf7c89b49930920f8f6c4d7bdf00331
Maximilian Coghlan <max.coghlan@icloud.com>
2020-11-08 00:28:12 +0000

    restructured javascript and pug files for mobile menu => halved the code

--- src/js/mobileMenu.js
+++ src/js/mobileMenu.js
@@ -1,3 +1,5 @@
+// SHOW | HIDE MENU
+
 const menuBtn = document.querySelector(".menu-btn");
 const hamburger = document.querySelector(".menu-btn__burger");
 const mobileMenu = document.querySelector(".mobile-menu__menu");
@@ -21,10 +23,19 @@ function toggleMenu() {
 }
 
 // MENU FUNCTIONALITY
+
 // Get Page Menu
-const originalMenu = document.querySelector(".secondary-nav__list");
+const originalMenu = document.getElementsByClassName(
+  "secondary-nav__list original"
+)[0];
 let originalMenuId = originalMenu.id;
 const menuContainer = document.querySelector(".mobile-menu__secondary-nav");
+
+// Get Menus
+const eventsMenu = document.querySelector("#events-list");
+const designMenu = document.querySelector("#design-list");
+const marketingMenu = document.querySelector("#marketing-list");
+const moreMenu = document.querySelector("#more-list");
 
 // Nav Buttons
 const eventsBtn = document.querySelector("#main-menu__events");
@@ -33,20 +44,17 @@ const marketingBtn = document.querySelector("#main-menu__marketing");
 const moreBtn = document.querySelector("#main-menu__more");
 const allButtons = document.querySelectorAll(".main-nav__list-item");
 
-// Original Menu
+// Current Menu
 let currentMenuId = originalMenuId;
 let currentMenu = originalMenu;
 
-// REQUIRED VARIABLES
-let eventsMenu, designMenu, marketingMenu, moreMenu, newMenu;
-
-// EVENT LISTENERS
+// Event Listeners
 eventsBtn.addEventListener("click", handleEventsMenu);
 designBtn.addEventListener("click", handleDesignMenu);
 marketingBtn.addEventListener("click", handleMarketingMenu);
 moreBtn.addEventListener("click", handleMoreMenu);
 
-// HANDLERS
+// Handlers
 function handleEventsMenu() {
   if (currentMenuId !== "events-list") {
     allButtons.forEach((btn) => btn.classList.remove("active"));
@@ -55,15 +63,7 @@ function handleEventsMenu() {
     removeCurrentMenu();
 
     if (originalMenuId !== "events-list") {
-      if (eventsMenu) {
-        showCreatedMenu(eventsMenu, "events-list");
-      } else {
-        createAndShowMenu(
-          createEventsMenu,
-          "#events-menu-overlay",
-          "events-list"
-        );
-      }
+      showSubMenu(eventsMenu, "events-list");
     } else {
       showOriginalMenu();
     }
@@ -77,21 +77,12 @@ function handleDesignMenu() {
     removeCurrentMenu();
 
     if (originalMenuId !== "design-list") {
-      if (designMenu) {
-        showCreatedMenu(designMenu, "design-list");
-      } else {
-        createAndShowMenu(
-          createDesignMenu,
-          "#design-menu-overlay",
-          "design-list"
-        );
-      }
+      showSubMenu(designMenu, "design-list");
     } else {
       showOriginalMenu();
     }
   }
 }
-
 function handleMarketingMenu() {
   if (currentMenuId !== "marketing-list") {
     allButtons.forEach((btn) => btn.classList.remove("active"));
@@ -100,21 +91,12 @@ function handleMarketingMenu() {
     removeCurrentMenu();
 
     if (originalMenuId !== "marketing-list") {
-      if (marketingMenu) {
-        showCreatedMenu(marketingMenu, "marketing-list");
-      } else {
-        createAndShowMenu(
-          createMarketingMenu,
-          "#marketing-menu-overlay",
-          "marketing-list"
-        );
-      }
+      showSubMenu(marketingMenu, "marketing-list");
     } else {
       showOriginalMenu();
     }
   }
 }
-
 function handleMoreMenu() {
   if (currentMenuId !== "more-list") {
     allButtons.forEach((btn) => btn.classList.remove("active"));
@@ -123,11 +105,7 @@ function handleMoreMenu() {
     removeCurrentMenu();
 
     if (originalMenuId !== "more-list") {
-      if (moreMenu) {
-        showCreatedMenu(moreMenu, "more-list");
-      } else {
-        createAndShowMenu(createMoreMenu, "#more-menu-overlay", "more-list");
-      }
+      showSubMenu(moreMenu, "more-list");
     } else {
       showOriginalMenu();
     }
@@ -141,29 +119,13 @@ function removeCurrentMenu() {
   setTimeout(() => currentMenu.classList.add("remove"), 350);
 }
 
-function showCreatedMenu(menu, menuId) {
+function showSubMenu(menu, menuId) {
   setTimeout(() => menu.classList.remove("remove"), 350);
 
   setTimeout(() => {
     menu.classList.remove("fade-out");
     currentMenuId = menuId;
     currentMenu = menu;
-  }, 400);
-}
-
-function createAndShowMenu(cb, querySelector, menuId) {
-  setTimeout(() => cb(), 350);
-
-  setTimeout(() => {
-    newMenu = document.querySelector(querySelector);
-    newMenu.classList.remove("fade-out");
-    currentMenuId = menuId;
-    currentMenu = newMenu;
-
-    if (menuId === "events-list") eventsMenu = newMenu;
-    if (menuId === "marketing-list") marketingMenu = newMenu;
-    if (menuId === "design-list") designMenu = newMenu;
-    if (menuId === "more-list") moreMenu = newMenu;
   }, 400);
 }
 
@@ -174,100 +136,4 @@ function showOriginalMenu() {
     currentMenuId = originalMenuId;
     currentMenu = originalMenu;
   }, 400);
-}
-
-// CREATE MENUS
-
-function createEventsMenu() {
-  const subItems = [
-    { name: "Events", link: "/events.html" },
-    { name: "Event Management", link: "/event-management.html" },
-    { name: "Markets", link: "/markets.html" },
-  ];
-  const list = document.createElement("ul");
-  list.classList.add("secondary-nav__list");
-  list.id = "events-menu-overlay";
-  list.classList.add("fade-out");
-  subItems.forEach((item) => {
-    const listItem = document.createElement("li");
-    const link = document.createElement("a");
-    link.setAttribute("href", item.link);
-    link.innerText = item.name;
-
-    listItem.appendChild(link);
-    list.appendChild(listItem);
-    menuContainer.appendChild(list);
-  });
-}
-
-function createDesignMenu() {
-  const subItems = [
-    { name: "Design", link: "/graphic-design.html" },
-    { name: "Website Design", link: "/website-design.html" },
-    { name: "Design for Print", link: "/design-for-print.html" },
-    { name: "Logo Design", link: "/logo-design.html" },
-  ];
-  const list = document.createElement("ul");
-  list.classList.add("secondary-nav__list");
-  list.id = "design-menu-overlay";
-  list.classList.add("fade-out");
-  subItems.forEach((item) => {
-    const listItem = document.createElement("li");
-    const link = document.createElement("a");
-    link.setAttribute("href", item.link);
-    link.innerText = item.name;
-
-    listItem.appendChild(link);
-    list.appendChild(listItem);
-    menuContainer.appendChild(list);
-  });
-}
-
-function createMarketingMenu() {
-  const subItems = [
-    { name: "Marketing", link: "/marketing.html" },
-    { name: "Workshops", link: "/workshops.html" },
-    { name: "Freelance", link: "/freelance-marketing.html" },
-    { name: "Social Media", link: "/social-media.html" },
-    { name: "Copywriting", link: "/copywriting-services.html" },
-    { name: "Consultancy", link: "/consultancy.html" },
-  ];
-  const list = document.createElement("ul");
-  list.classList.add("secondary-nav__list");
-  list.id = "marketing-menu-overlay";
-  list.classList.add("fade-out");
-  subItems.forEach((item) => {
-    const listItem = document.createElement("li");
-    const link = document.createElement("a");
-    link.setAttribute("href", item.link);
-    link.innerText = item.name;
-
-    listItem.appendChild(link);
-    list.appendChild(listItem);
-    menuContainer.appendChild(list);
-  });
-}
-
-function createMoreMenu() {
-  const subItems = [
-    { name: "Pricing", link: "/pricing.html" },
-    { name: "Privacy", link: "/privacy.html" },
-    { name: "Contact", link: "/contact-us.html" },
-    { name: "Site Map", link: "/sitemap.html" },
-    { name: "Testimonials", link: "/testimonials.html" },
-  ];
-  const list = document.createElement("ul");
-  list.classList.add("secondary-nav__list");
-  list.id = "more-menu-overlay";
-  list.classList.add("fade-out");
-  subItems.forEach((item) => {
-    const listItem = document.createElement("li");
-    const link = document.createElement("a");
-    link.setAttribute("href", item.link);
-    link.innerText = item.name;
-
-    listItem.appendChild(link);
-    list.appendChild(listItem);
-    menuContainer.appendChild(list);
-  });
 }
