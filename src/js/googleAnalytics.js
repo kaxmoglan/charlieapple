06c5c4e952bceab37016679e4daf9bd90182252d
Maximilian Coghlan <max.coghlan@icloud.com>
2020-11-09 18:43:11 +0000

    footer social icons spacing safari, contact form title and top margin on mobile, potential send button styling fix on ios, image attributes

---
+++ src/js/googleAnalytics.js
@@ -0,0 +1,7 @@
+window.dataLayer = window.dataLayer || [];
+function gtag() {
+  dataLayer.push(arguments);
+}
+gtag("js", new Date());
+
+gtag("config", "UA-31084152-1", { cookie_flags: "SameSite=None;Secure" });
